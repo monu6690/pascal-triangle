7ce2ef6d5713122b23a8f13176d4311ee058c02e
manish <manishmehta.monu@gmail.com>
2019-02-03 01:17:51 +0530

    added

---
+++ SortArray.java
@@ -0,0 +1,52 @@
+import java.io.*; 
+import java.util.*; 
+
+class Pair { 
+    int x; 
+    int y; 
+ 
+public Pair(int x, int y) 
+    { 
+        this.x = x; 
+        this.y = y; 
+    } 
+} 
+ 
+class Compare { 
+  
+    static void compare(Pair arr[], int n) 
+    { 
+        Arrays.sort(arr, new Comparator<Pair>() { 
+            @Override public int compare(Pair p1, Pair p2) 
+            { 
+                return p1.y - p2.y; 
+            } 
+        }); 
+  
+        for (int i = 0; i < n; i++) { 
+            System.out.print(arr[i].x + " " + arr[i].y + " "); 
+        } 
+        System.out.println(); 
+    } 
+}  
+class SortArray { 
+  
+public static void main(String[] args) 
+    { 
+        Scanner sc = new Scanner(System.in); 
+  
+        // length of array 
+        int n = 5; 
+  
+        // Array of Pair 
+        Pair arr[] = new Pair[n]; 
+  
+        arr[0] = new Pair(10, 20); 
+        arr[1] = new Pair(1, 2); 
+        arr[2] = new Pair(3, 1); 
+        arr[3] = new Pair(10, 8); 
+        arr[4] = new Pair(4, 3); 
+        Compare obj = new Compare(); 
+  
+        obj.compare(arr, n); 
+    } 
